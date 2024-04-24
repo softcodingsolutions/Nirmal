baf7aa504e3daf9c85e6ae6ca2ed101246361ee3
Nirmal Vaja <nirmal@softcodingsolutions.com>
2024-04-24 16:58:06 +0530

    added check for the argument to be only string. Will raise argumentError if string not passed.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,17 @@ describe StringCalculator do
         expect(StringCalculator.add('1,3,5,6')).to eq(15)
       end
     end
+
+    context 'Valid string is passed as an argument' do
+      it 'expects only one string argument' do
+        expect{ StringCalculator.add('11') }.not_to raise_error(ArgumentError, 'The method signature permits only one argument, which must be a string.')
+      end
+    end
+
+    context 'Number is passed as an argument' do
+      it 'expects only on string argument' do
+        expect { StringCalculator.add(11).to raise_error(ArgumentError, 'The method signature permits only one argument, which must be a string.') }
+      end
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 class StringCalculator
   def self.add(numbers)
+    return ArgumentError, 'The method signature permits only one argument, which must be a string.' unless numbers.is_a?(String)
     return 0 if numbers.empty?
 
     numbers.split(",").map(&:to_i).sum
