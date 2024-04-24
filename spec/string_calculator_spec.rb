c0ec3bd2b54579fe240158e7cba349f9a4ae0068
Nirmal Vaja <nirmal@softcodingsolutions.com>
2024-04-24 16:30:56 +0530

    Summing up the numbers for the non empty strings

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,15 @@ describe StringCalculator do
         expect(StringCalculator.add('')).to eq 0
       end
     end
+
+    context 'when string is passed with numbers' do
+      it "returns the number itself for a single number" do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+
+      it "returns the sum of two numbers" do
+        expect(StringCalculator.add("1,5")).to eq(6)
+      end
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -5,5 +5,7 @@ require 'pry'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
+
+    numbers.split(",").map(&:to_i).sum
   end
 end
